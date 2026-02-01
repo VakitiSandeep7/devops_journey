691c3a6f136428c066210213265a443d0fc9ec5e
sandeep vakiti <vakitisandeep9966@gmail.com>
2026-02-01 15:50:09 +0530

    upgrade: Added for-loop to monitor multiple inventory files

--- check_system.sh
+++ check_system.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo "--- DevOps System Check ---"
-# We use $(...) to capture the output of the command into a variable
-LINES=$(wc -l < inventory.txt)
+echo "--- DevOps Multi-Inventory Check ---"
+# This loop only grabs files stsrting with 'inventory'
+for file in inventory*.txt; do
+    # Count lines of the CURRENT file in the loop
+    LINES=$(wc -l < "$file")
 
-if [ $LINES -gt 5 ]; then
-    echo "Status: WARNING - Inventory is getting full ($LINES items)!"
-else
-    echo "Status: OK - plenty of space left ($LINES items)!"
-fi
+    if [ $LINES -gt 5 ]; then
+        echo "Status: WARNING - $file is full ($LINES items)!"
+    else
+        echo "Status: OK - $file looks good ($LINES items)."
+    fi
+done
 echo "---------------------------"
